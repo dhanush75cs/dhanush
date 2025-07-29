0a320b98178d13a37398cb5301dfec74d2ab38c0
dhanush75cs <dhanush75dd@gmail.com>
2025-07-29 11:59:10 +0530

    Add files via upload

---
+++ javascript calculator/index.js
@@ -0,0 +1,17 @@
+const display=document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+function clearDisplay(){
+    display.value="";
+}
+
+function Calculate(){
+    try{
+        display.value=eval(display.value);
+    }
+    catch(errror){
+        display.value="Error"
+    }
+}
